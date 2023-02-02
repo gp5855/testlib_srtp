1bc3de5abfb69085f00c6da0a29495ac0a718b10
gp5855 <gagan.patil@zebra.com>
2023-02-02 14:00:28 +0530

    changes wrt com.example.ndklibsrtp

--- libsrtp_test.cpp
+++ libsrtp_test.cpp
@@ -4,7 +4,7 @@
 
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_libsrtpcpp_MainActivity_stringFromJNI(JNIEnv *env,
+Java_com_example_ndklibsrtp_MainActivity_stringFromJNI(JNIEnv *env,
                                                        jobject /* this */)
 {
     std::string hello = "Hello from C++";
